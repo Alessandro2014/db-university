27e8c47d82100e59e85e1a3ae6dcd0d402cd38ee
Alessandro2014 <alessandromerenda22@gmail.com>
2021-09-29 08:11:52 +0200

    ufficio insegnanti

--- query_GROUP_BY.sql
+++ query_GROUP_BY.sql
@@ -2,3 +2,8 @@
 SELECT COUNT(*) AS `student`, YEAR(`enrolment_date`) as `year` 
 FROM `students` 
 GROUP BY `year`;
+
+--  Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(`id`), `office_number` 
+FROM `teachers` 
+GROUP BY `office_number`;
