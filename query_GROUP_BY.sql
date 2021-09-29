5953e50b119148b246ca1cfaf4972ea8b6bfa7ac
Alessandro2014 <alessandromerenda22@gmail.com>
2021-09-29 08:47:45 +0200

    corsi di laurea per dipartimento

--- query_GROUP_BY.sql
+++ query_GROUP_BY.sql
@@ -7,3 +7,13 @@ GROUP BY `year`;
 SELECT COUNT(`id`), `office_number` 
 FROM `teachers` 
 GROUP BY `office_number`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT `exam_id`, ROUND(AVG(`vote`), 2) AS `average_vote` 
+FROM `exam_student` 
+GROUP BY `exam_id`
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(*) AS `degree_courses`, `department_id` AS `single_department`
+FROM `degrees`
+GROUP BY `single_department`;
