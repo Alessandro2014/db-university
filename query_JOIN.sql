926feda3dd68a793edb11369590d012b47def210
Alessandro2014 <alessandromerenda22@gmail.com>
2021-09-30 13:53:16 +0200

    es 7

--- query_JOIN.sql
+++ query_JOIN.sql
@@ -14,34 +14,41 @@ ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` LIKE '%Neuroscienze%';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-SELECT `courses`.*, `teachers`.`name` AS `name_of_teacher`, `teachers`.`surname` AS `surname_of_teacher`
+SELECT `courses`.*
 FROM `courses`
-INNER JOIN `teachers`
-ON `teachers`.`id` = `courses`.`id`
+INNER JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers` ON `teachers`.`id` = `course_teacher`.`teacher_id`
 WHERE `teachers`.`name` = 'Fulvio'
-AND`teachers`.`surname` = 'Amato';
+AND`teachers`.`surname` = 'Amato'
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
--- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
--- SELECT `movies`.*, `categories`.* FROM `movies` LEFT JOIN `categories` ON `movies`.`category_id` = `categories`.`id`
--- `students`.`degree_id`,`courses`.`degree_id`
-SELECT `students`.`name`, `students`.`surname`, `students`.`fiscal_code`, `courses`.`name` AS `nome_corso`, `degrees`.`name` AS `nome_degree`
-FROM `degrees` 
-LEFT JOIN `students` 
-ON `degrees`.`id` = `students`.`degree_id`
-JOIN `courses`
-ON `degrees`.`id` = `courses`.`degree_id`
-WHERE `students`.`degree_id` = `courses`.`degree_id`
+SELECT `students`.`name`, `students`.`surname`, `students`.`fiscal_code`, `departments`.`name`, `degrees`.`name`
+FROM `students` 
+JOIN `degrees` ON `degrees`.`id` = `students`.`degree_id`
+JOIN `departments` ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`name`, `students`.`surname`
+
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-SELECT `teachers`.`name` AS `teacher_name`, `teachers`.`surname` AS `teacher_surname`, `teachers`.`phone`, `degrees`.*, `courses`.`name` AS `course_name` 
+SELECT `teachers`.`name` AS `teacher_name`, `teachers`.`surname` AS `teacher_surname`, `degrees`.*, `courses`.`name` AS `course_name` 
 FROM `degrees`
-JOIN `courses` ON `degrees`.`id` = `courses`.`degree_id`
-JOIN `teachers` ON `teachers`.`id` = `courses`.`id`;
+JOIN `courses` ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers` ON `teachers`.`id` = `course_teacher`.`teacher_id`
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
-SELECT `teachers`.*, `departments`.`name` AS `nome_dipartimento` 
-FROM `courses`
-JOIN `teachers` ON `courses`.`id` = `teachers`.`id`
-JOIN `degrees` ON `courses`.`degree_id` = `degrees`.`id`
-JOIN `departments`ON `degrees`.`department_id` = `departments`.`id`
+SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`, `departments`.`name` AS `name_department` 
+FROM `teachers`
+JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id` 
+JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `departments`ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Matematica'
+ORDER BY `teachers`.`name`, `teachers`.`surname`
+
+-- 7. Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT `students`.`name`, `students`.`surname`, `students`.`fiscal_code`, `courses`.`name`, COUNT(`exam_student`.`vote`) AS `total_attempts`,
+MAX(`exam_student`.`vote`) AS `max_vote`
+FROM `students` 
+JOIN `exam_student` ON `students`.`id` = `exam_student`.`student_id`
+JOIN `exams` ON `exams`.`id` = `exam_student`.`exam_id`
+JOIN `courses` ON `courses`.`id` = `exams`.`course_id`
+GROUP BY `students`.`id`, `courses`.`id`
